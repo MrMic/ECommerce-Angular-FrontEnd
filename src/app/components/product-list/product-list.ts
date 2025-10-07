0f1dd5926390625071fc8d75695ffa1f9ddf9c27
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-10-07 10:43:08 +0200

    feat: implement dynamic product list rendering
    - Add products iteration in product-list.html
    - Fetch product list from ProductService in product-list.ts
    - Integrate CurrencyPipe for price formatting

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product';
+import { Product } from '../../common/product';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-list.html',
-  styleUrl: './product-list.css'
+  styleUrl: './product-list.css',
 })
-export class ProductList {
+export class ProductList implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
