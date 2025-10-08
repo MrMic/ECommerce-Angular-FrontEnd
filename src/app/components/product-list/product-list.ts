35e0a97086fc0b7627ec517367b1b3120a073fbd
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-10-08 10:42:47 +0200

    feat: add product list table component
    - Create product-list-table.html for displaying product data
    - Update ProductList to use the new table template
    - Comment out the old template reference in product-list.ts

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -7,13 +7,14 @@ import { CurrencyPipe } from '@angular/common';
   selector: 'app-product-list',
   standalone: true,
   imports: [CurrencyPipe],
-  templateUrl: './product-list.html',
+  templateUrl: './product-list-table.html',
+  // templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
 export class ProductList implements OnInit {
   products: Product[] = [];
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
     this.listProducts();
