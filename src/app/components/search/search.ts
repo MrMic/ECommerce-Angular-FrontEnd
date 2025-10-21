abfce31519abb376fc328d772a08966c72a440b8
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-10-21 12:57:07 +0200

    feat: replace inline search with reusable search component - 96 / Send data to search route
    - Add app-search component to app.html
    - Integrate Search module in app.ts imports
    - Build functional search UI in search.html
    - Add navigation logic to search.ts

--- src/app/components/search/search.ts
+++ src/app/components/search/search.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   imports: [],
   templateUrl: './search.html',
-  styleUrl: './search.css'
+  styleUrl: './search.css',
 })
 export class Search {
+  constructor(private router: Router) { }
 
+  doSearch(value: string) {
+    console.log(`value=${value}`);
+    this.router.navigateByUrl(`/search/${value}`);
+  }
 }
