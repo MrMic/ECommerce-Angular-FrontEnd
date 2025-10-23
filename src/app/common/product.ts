97f24427eb32cf144664e57dc0b2bbb0eb51d527
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-10-23 10:55:30 +0200

    feat: add product details component and route - 101 / ProductDetail Component + route update
    - Create ProductDetails component and associated files
    - Add route for product details in app.routes.ts
    - Update Product class to include id property

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -1,5 +1,6 @@
 export class Product {
   constructor(
+    public id: number,
     public sku: string,
     public name: string,
     public description: string,
@@ -9,5 +10,5 @@ export class Product {
     public unitsInStock: number,
     public dateCreated: Date,
     public lastUpdated: Date,
-  ) {}
+  ) { }
 }
