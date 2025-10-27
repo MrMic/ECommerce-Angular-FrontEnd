2030fa43c3d7086de05706f6b1aa9d5e6b50cafe
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-10-27 12:50:24 +0100

    feat: Install of @ng-bootstrap for providing NgbModule ( Pagination )

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ProductCategoryMenu } from './components/product-category-menu/product-category-menu';
 import { Search } from './components/search/search';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductCategoryMenu, Search],
+  imports: [RouterOutlet, ProductCategoryMenu, Search, NgbModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
