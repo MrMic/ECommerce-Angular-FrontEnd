1e0286d0c0bbfcb2fbf7124abd0fb053499a8698
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-11-03 11:00:03 +0100

    feat: improve product search and pagination - 120 / Keyword update ProductList Component
    - Add keyword comparison to reset page number on new search
    - Refactor to reuse processResult for data handling
    - Replace searchProducts with searchProductPaginate

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -23,6 +23,8 @@ export class ProductList implements OnInit {
   thePageSize: number = 10;
   theTotalElements: number = 0;
 
+  previousKeyword = '';
+
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute,
@@ -50,10 +52,20 @@ export class ProductList implements OnInit {
   handleSearchProducts() {
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
 
+    // if we have a different keyword than previous
+    // then set thePageNumber to 1
+    if (this.previousKeyword != theKeyword) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousKeyword = theKeyword;
+
+    console.log(`keyword=${theKeyword}, thePageNumber=${this.thePageNumber}`);
+
     // now search for the products using keyword
-    this.productService.searchProducts(theKeyword).subscribe((data) => {
-      this.products = data;
-    });
+    this.productService
+      .searchProductPaginate(this.thePageNumber - 1, this.thePageSize, theKeyword)
+      .subscribe(this.processResult());
   }
 
   //   ______________________________________________________________________
@@ -84,12 +96,7 @@ export class ProductList implements OnInit {
     // now get the products for the given category id
     this.productService
       .getProductListPaginate(this.thePageNumber - 1, this.thePageSize, this.currentCategoryId)
-      .subscribe((data) => {
-        this.products = data._embedded.products;
-        this.thePageNumber = data.page.number + 1;
-        this.thePageSize = data.page.size;
-        this.theTotalElements = data.page.totalElements;
-      });
+      .subscribe(this.processResult());
   }
 
   //   ______________________________________________________________________
@@ -97,5 +104,14 @@ export class ProductList implements OnInit {
     this.thePageSize = +pageSize;
     this.thePageNumber = 1;
     this.listProducts();
+  }
+
+  processResult() {
+    return (data: any) => {
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.totalElements;
+    };
   }
 }
