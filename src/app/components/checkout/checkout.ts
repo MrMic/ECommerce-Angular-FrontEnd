17ac0e4beddc0084f98cb098c61e71f56eda9846
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-12-01 14:49:36 +0100

    feat: enhance checkout form functionality - 153/ Add Event Handling for Submit
    - Add onSubmit handler for form submission
    - Fix typo in "lastName" form control
    - Update heading text to "Customer"
    - Adjust spacing in component metadata for readability

--- src/app/components/checkout/checkout.ts
+++ src/app/components/checkout/checkout.ts
@@ -2,22 +2,30 @@ import { Component } from "@angular/core";
 import { FormBuilder, FormGroup, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
-	selector: "app-checkout",
-	standalone: true,
-	imports: [ReactiveFormsModule],
-	templateUrl: "./checkout.html",
-	styleUrl: "./checkout.css",
+  selector: "app-checkout",
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: "./checkout.html",
+  styleUrl: "./checkout.css",
 })
 export class Checkout {
-	checkoutFormGroup: FormGroup;
+  checkoutFormGroup: FormGroup;
 
-	constructor(private formBuilder: FormBuilder) {
-		this.checkoutFormGroup = this.formBuilder.group({
-			customer: this.formBuilder.group({
-				firstName: [""],
-				lastName: [""],
-				email: [""],
-			}),
-		});
-	}
+  // * INFO: CONSTRUCTOR __________________________________________________
+  constructor(private formBuilder: FormBuilder) {
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [""],
+        lastName: [""],
+        email: [""],
+      }),
+    });
+  }
+
+  // ______________________________________________________________________
+  onSubmit() {
+    console.log("Handling the submit button");
+    console.log(this.checkoutFormGroup.get("customer")?.value);
+    console.log("Email is: " + this.checkoutFormGroup.get("customer")?.value.email);
+  }
 }
