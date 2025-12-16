cd2d69f1ce1deff22364b7ca656a520365fd4f59
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-12-16 15:25:16 +0100

    feat(checkout): dynamically update credit card expiration months - 163/
    Implemented dynamic updating of credit card expiration months based on
    the selected year in the checkout form.
    
    - Added a (change) event listener to the expiration year select element
    in checkout.html.
    - Introduced handleMonthsAndYears() method in checkout.ts to calculate
    and update the available months.
    - This ensures that past months are not selectable when the current year
    is chosen, improving the user experience and form validation.

--- src/app/components/checkout/checkout.ts
+++ src/app/components/checkout/checkout.ts
@@ -87,4 +87,25 @@ export class Checkout {
     console.log('Handling ths submit group');
     console.log(this.checkoutFormGroup.get('customer')?.value);
   }
+
+  // ______________________________________________________________________
+  handleMonthsAndYears() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear);
+
+    let startMonth: number;
+
+    if (currentYear == selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    } else {
+      startMonth = 1;
+    }
+
+    this.luv2ShopFormService.getCreditCardMonths(startMonth).subscribe((data: number[]) => {
+      console.log('Retrieved credit card months: ' + JSON.stringify(data));
+      this.creditCardMonths = data;
+    });
+  }
 }
